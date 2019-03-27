f76780fc67b9137493ff2fc2d8fff6a07895d6f5
littledy <littledy@users.noreply.github.com>
2019-03-27 08:55:40 +0800

    Create bubble_sort.cpp

---
+++ bubble_sort.cpp
@@ -0,0 +1,48 @@
+﻿// bubble_sort.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+const int N = 9;
+
+void bubble_sort(int *p, int n)
+{
+	for (int i = 0;i < n;++i)
+	{
+		for (int j = n-1;j >0;--j)
+		{
+			if (p[j] < p[j - 1])
+			{
+				int temp = p[j - 1];
+				p[j - 1] = p[j];
+				p[j] = temp;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int a[N] = { 1,34,53,123,52,52,6,62,23 };
+
+	bubble_sort(a, N);
+
+	for (auto c : a)
+		cout << c << " ";
+
+	cout << endl;
+	return 0;
+
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
